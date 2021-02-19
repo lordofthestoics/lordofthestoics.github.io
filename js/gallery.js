69a94c6e26446c283516f44bee58666a155efc09
Lee Sushko <leesushko@gmail.com>
2021-02-18 20:13:27 -0500

    Preload large images

--- js/gallery.js
+++ js/gallery.js
@@ -4,25 +4,26 @@
 function activateGallery() {
 	let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
 	let mainImage = document.querySelector("#gallery-photo img");
-	// Image info to be updated
-	let galleryInfo = document.querySelector("#gallery-info");
-	let title		= galleryInfo.querySelector(".title");
-	let description = galleryInfo.querySelector(".description");
 
 	thumbnails.forEach(function(thumbnail) {
+		// Preload large images.
+		let newImageSrc = thumbnail.dataset.largeVersion;
+		let largeVersion = new Image();
+		largeVersion.src = newImageSrc;
+		
 		thumbnail.addEventListener("click", function() {
-			// Set clicked image as main image.
-			let newImageSrc = thumbnail.dataset.largeVersion;
-			let newImageAlt = thumbnail.dataset.description;
+			// Set clicked image as display image and alt.
 			mainImage.setAttribute("src", newImageSrc);
-			mainImage.setAttribute("alt", newImageAlt);
 			
 			// Change which image is current.
-			let currentClass = "current";
-			document.querySelector(".current").classList.remove(currentClass);
-			thumbnail.parentNode.classList.add(currentClass);
+			document.querySelector(".current").classList.remove("current");
+			thumbnail.parentNode.classList.add("current");
 			
 			// Update image info.
+			let galleryInfo = document.querySelector("#gallery-info");
+			let title 		= galleryInfo.querySelector(".title");
+			let description = galleryInfo.querySelector(".description");
+			
 			title.innerHTML			= thumbnail.dataset.title;
 			description.innerHTML	= thumbnail.dataset.description;
 		});
